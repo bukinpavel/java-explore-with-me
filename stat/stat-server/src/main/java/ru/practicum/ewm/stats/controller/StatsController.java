ad9f473b441377c25a7badaf8944a7b2d66db1f9
bukinpavel <bukinpavelvl@yandex.ru>
2023-05-24 00:11:32 +0400

    back-up version

--- stat/stat-server/src/main/java/ru/practicum/ewm/stats/controller/StatsController.java
+++ stat/stat-server/src/main/java/ru/practicum/ewm/stats/controller/StatsController.java
@@ -1,11 +1,28 @@
 package ru.practicum.ewm.stats.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.dto.stats.ViewStats;
+import ru.practicum.ewm.dto.stats.ViewsStatsRequest;
 import ru.practicum.ewm.stats.service.StatsService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class StatsController {
     private final StatsService statsService;
+
+    @PostMapping(value = "/hit")
+    public void addHit(@RequestBody ViewsStatsRequest request){
+        statsService.postHit(request);
+    }
+
+    @GetMapping(value = "/stats")
+    public List<ViewStats> getStats(@RequestParam("start") String start, @RequestParam("end") String end,
+                                    @RequestParam(value = "uris", required = false) String[] uris,
+                                    @RequestParam(value = " unique", required = false) String unique){
+        return statsService.getHit(start, end, uris, unique);
+    }
+
 }
