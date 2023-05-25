a062bf5f940d713f88da64db28df4fb596410fec
bukinpavel <bukinpavelvl@yandex.ru>
2023-05-25 17:11:46 +0400

    add-main-service-minimum

--- main-service/main-service-server/src/main/java/ru/practicum/MainServiceServerApp.java
+++ main-service/main-service-server/src/main/java/ru/practicum/MainServiceServerApp.java
@@ -3,6 +3,7 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MainServiceServerApp {
     public static void main(String[] args) {
