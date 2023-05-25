611197bacd37bb6b8f9a0db41f724ce7a91c1e0d
bukinpavel <bukinpavelvl@yandex.ru>
2023-05-25 21:00:16 +0400

    fixed-main-service-pom

--- main-service/main-service-server/src/main/java/ru/practicum/ewm/server/MainServiceServerController.java
+++ main-service/main-service-server/src/main/java/ru/practicum/ewm/server/MainServiceServerController.java
@@ -1,7 +1,9 @@
 package ru.practicum.ewm.server;
 
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.ewm.client.stats.StatsClient;
 
 @RestController
 public class MainServiceServerController {
+    //StatsClient statsClient = new StatsClient();
 }
