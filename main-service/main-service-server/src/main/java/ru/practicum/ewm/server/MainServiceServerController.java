291f769bad14242ba2cfd9c562d3d2cf60ade255
bukinpavel <bukinpavelvl@yandex.ru>
2023-05-26 11:08:57 +0400

    fixed-server-password-in-appproperties

--- main-service/main-service-server/src/main/java/ru/practicum/ewm/server/MainServiceServerController.java
+++ main-service/main-service-server/src/main/java/ru/practicum/ewm/server/MainServiceServerController.java
@@ -1,7 +1,7 @@
 package ru.practicum.ewm.server;
 
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.ewm.client.stats.StatsClient;
+//import ru.practicum.ewm.client.stats.StatsClient;
 
 @RestController
 public class MainServiceServerController {
